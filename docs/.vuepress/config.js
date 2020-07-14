bcfdf8e632dd75c54d9a447bfccb0822e5e55d5e
Yu <312364705@qq.com>
2020-07-14 11:05:27 +0800

    no message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,29 @@ const path = require('path')
 module.exports = {
   title: '前端知识地图',
   description: '前端知识地图',
-  plugins: ['autobar'],
+  // plugins: ['autobar'],
   themeConfig: {
-    // sidebar: 'auto'
+    sidebar: [
+      {
+        // 必要的
+        title: '灵魂拷问',
+        // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        path: '/灵魂拷问/',
+        // 可选的, 默认值是 true,
+        //collapsable: false,
+        // 可选的, 默认值是 1
+        //sidebarDepth: 1,
+        children: [
+          {
+            title: '浏览器',
+            children: {
+              title: '浏览器页面资源加载',
+              path: '/灵魂拷问/浏览器/'
+            },
+          },
+        ]
+      },
+    ]
   },
   configureWebpack: {
     resolve: {
